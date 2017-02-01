e976c9ebddef2bcd7a74efe173f598c1a909b3f0
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2017-02-01 15:51:42 +0700

    improvement

--- web/index.php
+++ web/index.php
@@ -1,4 +1,16 @@
 <?php
 
-echo 'sa ae tongkol';
+$hostname='database';
+$username='aden';
+$password='aden';
 
+try {
+    $dbh = new PDO("mysql:host=$hostname;dbname=aden",$username,$password);
+
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Connected to Database<br/>';
+
+    $dbh = null;
+} catch(\PDOException $e) {
+    echo $e->getMessage();
+}
